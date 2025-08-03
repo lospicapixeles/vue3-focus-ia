13a817d23c20a19780994fae334c79061c7368d7
Antony <antonyayansi@gmail.com>
2025-08-03 10:48:40 -0500

    as

--- src/apis/authApi.js
+++ src/apis/authApi.js
@@ -3,8 +3,8 @@ import axios from "axios";
 axios.defaults.withCredentials = true
 
 const authApi = axios.create({
-    baseURL: 'https://ali.credisur-peru.com/api',
-    //baseURL: 'http://localhost:9001/api',
+    // baseURL: 'https://ali.credisur-peru.com/api',
+    baseURL: 'http://localhost:9001/api',
 })
 
 export default authApi
